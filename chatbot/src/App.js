db9b8a538ef6813741760fe619d67c2982bd3bb0
gwelty24 <100785532+gwelty24@users.noreply.github.com>
2023-05-03 19:12:04 -0600

    Grace - updated the app.js. got the api key, bugs

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { Configuration, OpenAIApi } from 'openai'
+
+
 
 function App() {
+  
+   const configuration = new Configuration({
+     apiKey: process.env.REACT_APP_OPENAI_API_KEY
+   });
+
+   const openai = new OpenAIApi(configuration);
+
+   const generateImage = async () => {
+    const res = await openai.createImage({
+      prompt: "a white siamese cat",
+      n: 1,
+      size: "1024x1024",
+    });
+    console.log(res.data.data[0].url);
+  };
+  
   return (
     <div className="App">
-      
+      <button onClick={generateImage}>Generate Image</button>
     </div>
   );
 }
