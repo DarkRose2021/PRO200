3e89ca780f0db9a968ee5bd67e0dd1691736a7c2
DarkRose2021 <89149647+DarkRose2021@users.noreply.github.com>
2023-05-08 18:22:11 -0600

    Updated to scss and deleted the logos

--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,32 +1,27 @@
-import './App.css';
-import { Configuration, OpenAIApi } from 'openai'
-
-
-import logo from './logo.svg';
-import './App.scss';
+import { Configuration, OpenAIApi } from "openai";
+import "./App.scss";
 
 function App() {
-  
-   const configuration = new Configuration({
-     apiKey: process.env.REACT_APP_OPENAI_API_KEY
-   });
+	const configuration = new Configuration({
+		apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+	});
 
-   const openai = new OpenAIApi(configuration);
+	const openai = new OpenAIApi(configuration);
 
-   const generateImage = async () => {
-    const res = await openai.createImage({
-      prompt: "a white siamese cat",
-      n: 1,
-      size: "1024x1024",
-    });
-    console.log(res.data.data[0].url);
-  };
-  
-  return (
-    <div className="App">
-      <button onClick={generateImage}>Generate Image</button>
-    </div>
-  );
+	const generateImage = async () => {
+		const res = await openai.createImage({
+			prompt: "a white siamese cat",
+			n: 1,
+			size: "1024x1024",
+		});
+		console.log(res.data.data[0].url);
+	};
+
+	return (
+		<div className="App">
+			<button onClick={generateImage}>Generate Image</button>
+		</div>
+	);
 }
 
 export default App;
