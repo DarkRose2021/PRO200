ff33144b977b685493f7cc92895dbe2f4944f46c
gwelty24 <100785532+gwelty24@users.noreply.github.com>
2023-05-09 22:46:34 -0600

    Added a backend
    I added a backend, I deleted logos on the index.html, and a few things in the react part I commented out, but will use them in the backend

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+const port = 2000;
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+    res.send("Hey");
+});
+
+app.listen(port, () => {console.log(`Express listening on port ${port}`)});
--- chatbot/src/App.js
+++ chatbot/src/App.js
@@ -1,25 +1,26 @@
+
 import { Configuration, OpenAIApi } from "openai";
 import "./App.scss";
 
-function App() {
+ function App() {
 	const configuration = new Configuration({
 		apiKey: process.env.REACT_APP_OPENAI_API_KEY,
 	});
 
 	const openai = new OpenAIApi(configuration);
 
-	const generateImage = async () => {
-		const res = await openai.createImage({
-			prompt: "a white siamese cat",
-			n: 1,
-			size: "1024x1024",
-		});
-		console.log(res.data.data[0].url);
-	};
+	// const generateImage = async () => {
+		// const res = await openai.createImage({
+		// 	prompt: "a white siamese cat",
+		// 	n: 1,
+		// 	size: "1024x1024",
+		// });
+		// console.log(res.data.data[0].url);
+	// };
 
 	return (
 		<div className="App">
-			<button onClick={generateImage}>Generate Image</button>
+			{/* <button onClick={generateImage}>Generate Image</button> */}
 
 			<div className='container'>
 				<div>
